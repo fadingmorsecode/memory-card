30c57328f61fab708042f5f81d961e8b1f7987a6
Drake Williamson <williamson.drake@outlook.com>
2024-06-13 00:29:31 -0700

    create initial app structure

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+function PlayInfo({ onClick }) {
+  return (
+    <div>
+      <p>
+        PokéShuffle tests your memory. Your objective is to click on each card
+        only once. Can you remember which cards you&apos;ve already clicked?
+      </p>
+      <button onClick={onClick}>Close</button>
+    </div>
+  );
+}
+
+export default function App() {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
+
+  function handleCloseClick() {
+    setInfoIsOpen(false);
+  }
+
+  function handleOpenClick() {
+    setInfoIsOpen(true);
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>PokéShuffle</h1>
+        <p>Score: {score}</p>
+        <p>High Score: {highScore}</p>
+      </header>
+      <div>
+        <button onClick={handleOpenClick}>How To Play</button>
+        {infoIsOpen && <PlayInfo onClick={handleCloseClick} />}
+      </div>
+      <main>
+        <Gameboard />
+      </main>
+      <footer>Created by FadingMorseCode</footer>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import App from './components/App';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
